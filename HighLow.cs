6a803faf8d9ff8360a2823485550df5384762de8
Geoff Goetz and Jamison Cozart <geoff@>
2020-02-25 11:47:00 -0800

    Create initial files and directories as well all other code.

---
+++ HighLow.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+
+class HighLow
+{
+  public static void Main()
+  {
+    Console.WriteLine("Enter a number between 1-100");
+    int userInput = int.Parse(Console.ReadLine());
+    int high = 100;
+    int low = 0;
+    bool notCorrect = true;
+    while (notCorrect)
+    {
+      int guess = ((high + low) / 2);
+      Console.WriteLine("Did you guess " + guess + "?");
+      Console.WriteLine("Was my guess too high, too low or spot on?");
+      string userHighLowCorrect = Console.ReadLine();
+      if (userHighLowCorrect == "high")
+      {
+        high = guess;
+      }
+      else if (userHighLowCorrect == "low")
+      {
+        low = guess;
+      }
+      else if (userHighLowCorrect == "correct")
+      {
+        Console.WriteLine("Damn I'm good!");
+        notCorrect = false;
+      }
+    }
+  }
+}
